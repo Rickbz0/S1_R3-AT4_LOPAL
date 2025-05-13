fda8d0bf428674377ac3aecc7190c51aa91ab5ff
Henrique <Rickhfbo@gmail.com>
2025-05-13 12:52:14 -0300

    primeiro commit

--- index2.js
+++ index2.js
@@ -1,19 +1,19 @@
 let soma = 0;
-let contador = 0;
+let quantidade = 0;
 let nota;
 
 do {
   nota = parseFloat(prompt("Digite uma nota entre 0 e 10 ou um numero negativo para finalizar:"));
   if (!isNaN(nota) && nota >= 0 && nota <= 10) {
     soma += nota;
-    contador++;
+    quantidade++;
   } else if (nota >= 0) {
     alert("Nota invalida Digite um valor entre 0 e 10.");
   }
 } while (nota >= 0);
 
-if (contador > 0) {
-  let media = soma / contador;
+if (quantidade > 0) {
+  let media = soma / quantidade;
   alert(`A media das notas é: ${media.toFixed(2)}`);
 } else {
   alert("Nenhuma nota valida foi inserida.");
